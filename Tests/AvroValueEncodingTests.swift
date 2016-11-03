a9387896b9c481664eed6d7aeece08e71ac3b108
Jean-Alexis Montignies <ja_github@montignies.info>
2016-11-03 15:39:17 +0100

    Updated to Swifty JSON version, fixed test

--- Tests/AvroValueEncodingTests.swift
+++ Tests/AvroValueEncodingTests.swift
@@ -14,9 +14,9 @@ class AvroValueEncodingTests: XCTestCase {
         let jsonSchema = "{ \"type\" : \"enum\", \"name\" : \"ChannelKey\", \"doc\" : \"Enum of valid channel keys.\", \"symbols\" : [ \"CityIphone\", \"CityMobileWeb\", \"GiltAndroid\", \"GiltcityCom\", \"GiltCom\", \"GiltIpad\", \"GiltIpadSafari\", \"GiltIphone\", \"GiltMobileWeb\", \"NoChannel\" ]}"
         let schema = Schema(jsonSchema)
 
-        let correctAvroValue = AvroValue.AvroEnumValue(1, "CityMobileWeb")
-        let extraAvroValue = AvroValue.AvroEnumValue(10, "ExtraChannel")
-        let typoAvroValue = AvroValue.AvroEnumValue(5, "GiltiPad")
+        let correctAvroValue = AvroValue.avroEnumValue(1, "CityMobileWeb")
+        let extraAvroValue = AvroValue.avroEnumValue(10, "ExtraChannel")
+        let typoAvroValue = AvroValue.avroEnumValue(5, "GiltiPad")
         XCTAssertEqual(correctAvroValue.encode(schema)!, [0x2])
         XCTAssertNil(extraAvroValue.encode(schema), "")
         XCTAssertNil(typoAvroValue.encode(schema), "")
